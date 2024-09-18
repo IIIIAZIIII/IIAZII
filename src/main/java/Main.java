c311700978e4a3ded4aaafd877e24628d242a9f0
IIIIAZIIII <jeimsgameover228@gmail.com>
2024-09-19 02:19:29 +0700

    1zadanie

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("pop");
+        for (int i = 1; i <= 500; i++) {
+            System.out.println(i+":" +jojo(i));
+        }
     }
-}
+
+    public static String jojo(int gojo) {
+        if (gojo % 35 == 0) {
+            return "fizzbuzz";
+        } else if (gojo % 7 == 0) {
+            return "buzz";
+        } else if (gojo % 5 == 0) {
+            return "fizz";
+        } else {
+            return "";
+        }
+    }
+}            
