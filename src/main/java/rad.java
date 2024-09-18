427c9d9e26f732ee5f1b5d6685d24fe581feab7d
IIIIAZIIII <jeimsgameover228@gmail.com>
2024-09-19 03:17:06 +0700

    4zadanie

--- src/main/java/rad.java
+++ src/main/java/rad.java
@@ -5,11 +5,12 @@ public class rad {
     public static void main(String[] args) {
         System.out.println(rada());
     }
-    public static double rada(){
+
+    public static double rada() {
         int n = 2;
         double s = 0;
-        while ((1./(n*n+n-2.)) >= pow(10,-6)){
-            s+=(1./(n*n+n-2.));
+        while ((1. / (n * n + n - 2.)) >= pow(10, -6)) {
+            s += (1. / (n * n + n - 2.));
             n++;
         }
         return s;
