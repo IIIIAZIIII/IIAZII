c8e769e70d0204b1981003219a282e610781bf39
IIIIAZIIII <jeimsgameover228@gmail.com>
2024-09-19 12:03:51 +0700

    test-1

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -7,8 +7,8 @@ class MainTest {
     @Test
     void jojo() {
         assertEquals("fizzbuzz", Main.jojo(35));
-        assertEquals("buzz",Main.jojo(7));
-        assertEquals("fizz",Main.jojo(5));
-        assertEquals("",Main.jojo(1));
+        assertEquals("buzz", Main.jojo(7));
+        assertEquals("fizz", Main.jojo(5));
+        assertEquals("", Main.jojo(1));
     }
 }
